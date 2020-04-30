a29a22561d682a9379ea251c4e2793c0c218efae
sepy97 <sepy97@gmail.com>
2020-04-29 21:01:56 -0700

    now number of heaps works correctly

--- heap.cpp
+++ heap.cpp
@@ -14,23 +14,25 @@ llu fact 	(const unsigned int N, const unsigned int k);	//function calculates N!
 
 int main (int argc, char* argv [])
 {
+//	printf ("%d \n", choose (5, 3));
+
 	if (argc <= 2)
 	{
 		printf ("You should enter the opcode ('p' or 'n') and the size of a heap!\n");
 		return -1;
 	}
 
-	if (strcmp (argv[1], "n"))
+	if (!strcmp (argv[1], "n"))
 	{
 		unsigned int size = atoi (argv[2]);
-		printf ("%llu \n", numOfHeaps (size));
+		printf ("Number of heap-ordered trees of size %d: %llu \n", size, numOfHeaps (size));
 	}
-	else if (strcmp (argv[1], "p"))
+	else if (!strcmp (argv[1], "p"))
 	{
 		// TODO: check size boundaries (size >= 3)
 		unsigned int size = atoi (argv[2]);
 		
-		double result;// = numOfHeaps / 2.0;
+		double result = numOfHeaps*0.5;// / 2.0;
 		for (int i = 3; i <= size; i++)
 		{
 			result /= i;
@@ -45,12 +47,15 @@ int main (int argc, char* argv [])
 
 llu numOfHeaps (const unsigned int N)
 {
+//	printf ("now calculating heaps of size %d\n", N);
+	if (N <= 1) return 1;
+
 	llu result = 0;
-	for (int i = 1; i < N; i++)
+	for (int i = 1; i < N-1; i++)
 	{
 		result += choose (N, i) * numOfHeaps (i) * numOfHeaps (N-i-1);
 	}
-	return 0;
+	return result;
 }
 
 llu choose (const unsigned int N, const unsigned int k)
@@ -68,5 +73,10 @@ llu choose (const unsigned int N, const unsigned int k)
 
 llu fact (const unsigned int N, const unsigned int k)
 {
-	return 0;
+	llu result = 1;
+	for (int i = k; i < N; i++)
+	{
+		result *= (i+1);
+	}
+	return result;
 }
